33f1b60316e8e67795c5a2c6acf21f84467c83fc
Matt Garnett <14004106+lightclient@users.noreply.github.com>
2019-11-04 14:13:12 -0700

    add integration tests for new buffer host functions

--- tests/host_functions.rs
+++ tests/host_functions.rs
@@ -10,6 +10,10 @@ fn compile_wat(code: &str) -> Vec<u8> {
                     (import "env" "eth2_loadPreStateRoot" (func $load_pre_root (param i32)))
                     (import "env" "eth2_blockDataSize" (func $block_data_size (result i32)))
                     (import "env" "eth2_blockDataCopy" (func $block_data_copy (param i32) (param i32) (param i32)))
+                    (import "env" "eth2_bufferGet" (func $buffer_get (param i32) (param i32) (param i32) (result i32)))
+                    (import "env" "eth2_bufferSet" (func $buffer_set (param i32) (param i32) (param i32)))
+                    (import "env" "eth2_bufferMerge" (func $buffer_merge (param i32) (param i32)))
+                    (import "env" "eth2_bufferClear" (func $buffer_clear (param i32)))
                     (memory (export "memory") 1)
                     (func $main (export "main")
             "#,
@@ -83,4 +87,111 @@ fn block_data_copy() {
     let mut runtime = Runtime::new(&code, &block_data, [0u8; 32]);
     let post_root = runtime.execute();
     assert_eq!(post_root, build_root(42));
+}
+
+#[test]
+fn get_and_set_buffer() {
+    let code = compile_wat(
+        r#"
+            (i32.store (i32.const 32) (i32.const 42))
+            (call $buffer_set (i32.const 0) (i32.const 0) (i32.const 32))
+            (call $buffer_get (i32.const 0) (i32.const 0) (i32.const 64))
+            (drop)
+            (call $save_post_root (i32.const 64))
+        "#,
+    );
+
+    let mut runtime = Runtime::new(&code, &[], [0u8; 32]);
+    let post_root = runtime.execute();
+    assert_eq!(post_root, build_root(42));
+}
+
+#[test]
+fn merge_buffer() {
+    let code = compile_wat(
+        r#"
+            (i32.store (i32.const 0) (i32.const 1))
+            (i32.store (i32.const 32) (i32.const 1))
+
+            (i32.store (i32.const 64) (i32.const 2))
+            (i32.store (i32.const 96) (i32.const 2))
+
+            (i32.store (i32.const 128) (i32.const 2))
+            (i32.store (i32.const 160) (i32.const 3))
+
+            (i32.store (i32.const 192) (i32.const 4))
+            (i32.store (i32.const 224) (i32.const 4))
+
+            (call $buffer_set (i32.const 0) (i32.const 0) (i32.const 32))
+            (call $buffer_set (i32.const 0) (i32.const 64) (i32.const 96))
+            (call $buffer_set (i32.const 1) (i32.const 128) (i32.const 160))
+            (call $buffer_set (i32.const 1) (i32.const 192) (i32.const 224))
+
+            (call $buffer_merge (i32.const 0) (i32.const 1))
+
+            (call $buffer_get (i32.const 0) (i32.const 0) (i32.const 256))
+            (call $buffer_get (i32.const 0) (i32.const 64) (i32.const 288))
+            (call $buffer_get (i32.const 0) (i32.const 192) (i32.const 310))
+
+            (drop)
+            (drop)
+            (drop)
+
+
+            ;; Store the result of buffer[0,1] + buffer[0,2] + buffer[0,4] at mem[342]
+            (i32.store 
+                (i32.const 342)
+                (i32.add
+                    (i32.add (i32.load (i32.const 256)) (i32.load (i32.const 288)))
+                    (i32.add (i32.load (i32.const 310)) (i32.const 0))
+                )
+            )
+
+            (call $save_post_root (i32.const 342))
+        "#,
+    );
+
+    let mut runtime = Runtime::new(&code, &[], [0u8; 32]);
+    let post_root = runtime.execute();
+
+    // The post root should be 1 + 3 + 4 = 8
+    assert_eq!(post_root, build_root(8));
+}
+
+#[test]
+fn clear_buffer() {
+    let code = compile_wat(
+        r#"
+            (i32.store (i32.const 0) (i32.const 2))
+            (i32.store (i32.const 32) (i32.const 2))
+
+            (i32.store (i32.const 64) (i32.const 1))
+            (i32.store (i32.const 96) (i32.const 1))
+
+            (call $buffer_set (i32.const 0) (i32.const 0) (i32.const 32))
+            (call $buffer_set (i32.const 1) (i32.const 64) (i32.const 96))
+
+            (call $buffer_clear (i32.const 1))
+
+            (call $buffer_get (i32.const 0) (i32.const 0) (i32.const 128))
+            (call $buffer_get (i32.const 1) (i32.const 64) (i32.const 160))
+
+            (drop)
+            (drop)
+
+            ;; Store the result of buffer[0,1] + buffer[1,2] at mem[192]
+            (i32.store 
+                (i32.const 192)
+                (i32.sub (i32.load (i32.const 128)) (i32.load (i32.const 160)))
+            )
+
+            (call $save_post_root (i32.const 192))
+        "#,
+    );
+
+    let mut runtime = Runtime::new(&code, &[], [0u8; 32]);
+    let post_root = runtime.execute();
+
+    // The post root should be 2 - 0 = 2
+    assert_eq!(post_root, build_root(2));
 }
